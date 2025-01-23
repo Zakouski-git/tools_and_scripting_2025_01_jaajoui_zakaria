ca521ea92631af3528de004d45cd1a0e1dc927e9
Zakaria <zakaria.jaajoui@student.hepl.be>
2025-01-23 11:43:53 +0100

    Etape 5
    J'ai fait le code pour qu'un perso aléatoire devient rouge, amelioration du navmesh car le perso rentrait un peu dans les murs probleme au niveau du code : je n'ai pas réussi a trouver dans la documentation le fait que quand on clique ça envoit un raycast (pour l'agent), je sais qu'il existe car je l'avais utilisé en classe
    
    le perso ne bouge pas quand je met stop moving, il aurait fallu que je mette que c'est seulement quand il a fini son voyage vers le monolith qu'il peut utiliser la methode stopMoving, j'ai isolé le code pour faire bouger et pour faire revenir au point de base, les deux fonctionnent lorsqu'lls sont isolés

--- Assets/Script/Players Controller.cs
+++ Assets/Script/Players Controller.cs
@@ -1,43 +1,107 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
+
+       
+        /*void FixedUpdate()
+        {
+
+            RaycastHit hit;
+           
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+            }*/
 public class PlayersController : MonoBehaviour
 {
     [SerializeField] Transform [] charactersTab;
     [SerializeField] bool isMoving = false;
+    public float range = 30f;
+    private Transform initialPosition;
+    public Transform monolith;
     private Transform activePlayer;
+    NavMeshAgent agent;
     public Color initialColor = Color.white;
     public Color activeColor = Color.red;
-    
+    //LayerMask layerMask = LayerMask.GetMask("Monolith");
+
+
 
     void Start()
     {
-        
+       
     }
 
     
     void Update()
     {
-        if ( isMoving ==false)
+        //NavMeshHit hit;
+        
+        
+          if (isMoving == false && Input.GetMouseButtonDown(0))
         {
+            LayerMask mask = LayerMask.GetMask("Monolith");
+            /*if (!agent.Raycast(monolith.position, out hit))
+            {
+                Debug.Log("Hello: ");
+            }*/
             MovingPlayer();
         }
+            
+        
       
     }
 
     void MovingPlayer()
     {
+        isMoving = true;
         activePlayer = charactersTab[Random.Range(0, charactersTab.Length)];
         Renderer activeRedPlayer = activePlayer.GetComponent<Renderer>();
+        Transform initialPositioncharacter = activePlayer.GetComponent<Transform>();
+        initialPosition = initialPositioncharacter.transform;
+        agent = activeRedPlayer.GetComponent<NavMeshAgent>();
         activeRedPlayer.material.color = activeColor;
-        isMoving = true;
+        
+        agent.SetDestination(monolith.position);
+
+        //StartCoroutine(Wait());
+
+        if (agent.remainingDistance < range && isMoving == true)
+        {
+            StopMoving();
+        }
 
     }
-
-    void stopMoving()
+   
+   
+    /*IEnumerator Wait()
     {
+        yield return new WaitForSeconds(15);
+
+        
+            isMoving = false;
+            Renderer activeRedPlayer = activePlayer.GetComponent<Renderer>();
+            activeRedPlayer.material.color = initialColor;
+            agent.SetDestination(initialPosition.position);
+            Debug.Log("Hello: ");
+            
+           
+        
+    }*/
+    void StopMoving()
+    {
+        isMoving = false;
         Renderer activeRedPlayer = activePlayer.GetComponent<Renderer>();
         activeRedPlayer.material.color = initialColor;
+        agent.SetDestination(initialPosition.position);
+        Debug.Log("Hello: ");
+        
     }
 }
